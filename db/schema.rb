7bf3ee86a1ca01c05fd9c30e4183c3f0d03a2682
Wu <diwu@Sage5034.local>
2016-02-14 19:31:07 -0500

    test heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160213001537) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "simple_searches", force: :cascade do |t|
     t.string   "keywords"
     t.string   "location"
